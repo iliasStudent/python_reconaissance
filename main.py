8600f76f60c64f58f5e694a3bf47697ec3a1f379
iliasStudent <114088792+iliasStudent@users.noreply.github.com>
2023-12-16 15:35:58 +0100

    Updated pdf generator and colors

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
 from textwrap import wrap
+import json
 
 def whois_lookup(domain):
     try:
@@ -25,33 +26,44 @@ def ssl_certificate_info(domain):
                 return str(certificate)
     except Exception as e:
         return str(e)
+    
+def format_json(json_str):
+    try:
+        json_data = json.loads(json_str)
+        formatted_json = json.dumps(json_data, indent=2)
+        return formatted_json
+    except json.JSONDecodeError:
+        return json_str
 
 def create_pdf(domain, whois_info, ssl_info, output_dir):
     pdf_filename = os.path.join(output_dir, f"{domain}_scan_results.pdf")
     c = canvas.Canvas(pdf_filename, pagesize=letter)
-    width, height = letter  # Width and height of the page
+    width, height = letter
 
-    # Centered Title
+    # Centered Title with Blue Color
     title = f"Domain Scan Report for: {domain}"
     title_font_size = 16
     c.setFont("Helvetica-Bold", title_font_size)
+    c.setFillColor(colors.darkblue)  # Set color to dark blue for the title
     title_width = c.stringWidth(title, "Helvetica-Bold", title_font_size)
-    c.setFillColor(colors.darkblue)
-    c.drawString((width - title_width) / 2, 750, title)  # Center the title
+    c.drawString((width - title_width) / 2, 750, title)
+
+    # Reset color to black for the rest of the text
+    c.setFillColor(colors.black)
 
     # WHOIS Information
-    c.setFont("Helvetica", 12)
-    c.setFillColor(colors.black)
     y_position = 720
-    y_position = draw_wrapped_text(c, "WHOIS Information:", 50, y_position, 80)
-    y_position -= 15  # Add space between title and content
-    y_position = draw_wrapped_text(c, whois_info, 50, y_position, 80)
+    y_position = draw_wrapped_text(c, "WHOIS Information:", 50, y_position, 80, bold=True)
+    y_position -= 15
+    formatted_whois = format_json(whois_info)
+    y_position = draw_wrapped_text(c, formatted_whois, 50, y_position, 80)
 
     # SSL Certificate Information
-    y_position -= 30  # Add space between sections
-    y_position = draw_wrapped_text(c, "SSL Certificate Information:", 50, y_position, 80)
+    y_position -= 30
+    y_position = draw_wrapped_text(c, "SSL Certificate Information:", 50, y_position, 80, bold=True)
     y_position -= 15
-    y_position = draw_wrapped_text(c, ssl_info, 50, y_position, 80)
+    formatted_ssl = format_json(ssl_info)
+    y_position = draw_wrapped_text(c, formatted_ssl, 50, y_position, 80)
 
     c.save()
     return pdf_filename
@@ -65,7 +77,11 @@ def save_text_file(domain, whois_info, ssl_info, output_dir):
         file.write(ssl_info + "\n")
     return txt_filename
 
-def draw_wrapped_text(c, text, x, y, max_width):
+def draw_wrapped_text(c, text, x, y, max_width, font="Helvetica", font_size=12, bold=False):
+    if bold:
+        c.setFont("Helvetica-Bold", font_size)
+    else:
+        c.setFont(font, font_size)
     wrapped_text = wrap(text, max_width)
     for line in wrapped_text:
         c.drawString(x, y, line)
