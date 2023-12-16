4a5e3704e1fc68307cf345eaf353456289e81725
iliasStudent <114088792+iliasStudent@users.noreply.github.com>
2023-12-16 16:38:35 +0100

    Update pdf generator V2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from reportlab.lib import colors
 from textwrap import wrap
 import json
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 
 def whois_lookup(domain):
@@ -38,16 +38,24 @@ def format_json(json_str):
     except json.JSONDecodeError:
         return json_str
     
+def format_cell(text, style):
+    # Ensure text is a string
+    text = str(text) if text is not None else "None"
+    return Paragraph(text, style)
+
+    
 def parse_json_for_table(json_str):
     try:
         json_data = json.loads(json_str)
         table_data = [["Key", "Value"]]
         for key, value in json_data.items():
-            if isinstance(value, list):
-                value = ', '.join(str(v) for v in value)
+            if value is None:
+                value = "None"  # Convert None to a string
+            elif isinstance(value, list):
+                value = ', '.join(str(v) if v is not None else "None" for v in value)
             elif isinstance(value, dict):
                 value = json.dumps(value, indent=2)  # Nested JSON formatting
-            table_data.append([key, value])
+            table_data.append([key, str(value)])  # Ensure all values are strings
         return table_data
     except json.JSONDecodeError:
         return [["Error", "Invalid JSON data"]]
@@ -63,23 +71,31 @@ def create_pdf(domain, whois_info, ssl_info, output_dir):
     title_style.textColor = colors.darkblue
     title_style.alignment = 1  # Center alignment
 
+    # Custom style for table cells
+    cell_style = ParagraphStyle('CellStyle')
+    cell_style.wordWrap = 'CJK'  # Allows word wrapping in cells
+    cell_style.fontSize = 10
+    cell_style.leading = 12  # Space between lines
+
     # Title
     title = f"Domain Scan Report for: {domain}"
     elements.append(Paragraph(title, title_style))
 
-    # WHOIS Information Subtitle
+    # WHOIS Information Subtitle and Table
     elements.append(Paragraph("WHOIS Information:", styles['Heading2']))
     whois_data = parse_json_for_table(format_json(whois_info))
-    whois_table = Table(whois_data, colWidths=[doc.width/3.0, 2*doc.width/3.0])
+    whois_data_formatted = [[format_cell(cell, cell_style) for cell in row] for row in whois_data]
+    whois_table = Table(whois_data_formatted, colWidths=[doc.width/3.0, 2*doc.width/3.0])
     whois_table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
                                      ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
                                      ('ALIGN', (1, 1), (-1, -1), 'LEFT')]))
     elements.append(whois_table)
 
-    # SSL Certificate Information Subtitle
+    # SSL Certificate Information Subtitle and Table
     elements.append(Paragraph("SSL Certificate Information:", styles['Heading2']))
     ssl_data = parse_json_for_table(format_json(ssl_info))
-    ssl_table = Table(ssl_data, colWidths=[doc.width/3.0, 2*doc.width/3.0])
+    ssl_data_formatted = [[format_cell(cell, cell_style) for cell in row] for row in ssl_data]
+    ssl_table = Table(ssl_data_formatted, colWidths=[doc.width/3.0, 2*doc.width/3.0])
     ssl_table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
                                    ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
                                    ('ALIGN', (1, 1), (-1, -1), 'LEFT')]))
